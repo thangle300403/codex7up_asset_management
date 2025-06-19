cc68599d62288c2f450dc0fbb5f4298887132fcb
PhatDos <162654433+PhatDos@users.noreply.github.com>
2025-06-19 11:13:22 +0700

    Update AssetList.jsx

--- FE/src/pages/asset/AssetList.jsx
+++ FE/src/pages/asset/AssetList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+import { useSearch } from "../../context/SearchContext"; 
 
 const tableStyles = {  //CSS
   container: {
